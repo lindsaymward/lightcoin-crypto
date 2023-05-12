31bfd33b6e96dbface114104c20a2ced4d81fc1a
Lindsay Ward <lward23@hotmail.com>
2023-05-12 15:29:04 -0700

    Implement get method and classes for bank account transactions

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
-let balance = 500.00;
 
-class Withdrawal {
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-  constructor(amount) {
+  get balance() {
+    let balance = 0;
+    for (let transaction of this.transactions) {
+      balance += transaction.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount
+  }
+
+  isAllowed() {
+    return true
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -(this.amount);
+  }
+
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
   }
 
 }
@@ -15,15 +61,27 @@ class Withdrawal {
 
 
 
+
+
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25);
+const t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+const t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+const t4 = new Withdrawal(50.00, myAccount);
+t4.commit();
+console.log('Transaction 4:', t4);
+
+console.log('Balance:', myAccount.balance);
+console.log('Transaction History:', myAccount.transactions);
